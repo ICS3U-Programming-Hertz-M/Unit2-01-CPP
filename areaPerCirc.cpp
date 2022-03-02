a70088296021f083f45cefde685d20b971846e28
Ubuntu <ubuntu@ip-172-31-28-1.ec2.internal>
2022-03-02 04:09:02 +0000

    Added areaPerCirc.cpp

---
+++ areaPerCirc.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2021 Hertz Mukiza  All rights reserved.
+//
+// Created by: Hertz mukiza
+// Date: Feb. 22, 2022
+// This program uses libraries
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    // this function returns the interger portion of a float
+    std::cout << "the interger port of 12.4 is " << (floor(12.4)) << std::endl;
+}
